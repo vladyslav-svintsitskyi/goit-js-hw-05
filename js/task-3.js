c034734421ed8508fe4b012e0f320f937f19a382
Vladyslav Svintsitskyi <svintsitskyi1999@gmail.com>
2024-09-21 21:29:49 +0200

    add task

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,80 @@
-function checkForSpam(message) {
-  const checkedMsg = message.toLowerCase();
-  if (checkedMsg.includes("spam") || checkedMsg.includes("sale")) {
-    return true;
-  }
-  return false;
-}
+const sortByDescendingFriendCount = (users) => {
+  return users.toSorted((a, b) => b.friends.length - a.friends.length);
+};
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(
+  sortByDescendingFriendCount([
+    {
+      name: "Moore Hensley",
+      friends: ["Sharron Pace"],
+      gender: "male",
+    },
+    {
+      name: "Sharlene Bush",
+      friends: ["Briana Decker", "Sharron Pace"],
+      gender: "female",
+    },
+    {
+      name: "Ross Vazquez",
+      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+      gender: "male",
+    },
+    {
+      name: "Elma Head",
+      friends: ["Goldie Gentry", "Aisha Tran"],
+      gender: "female",
+    },
+    {
+      name: "Carey Barr",
+      friends: ["Jordan Sampson", "Eddie Strong"],
+      gender: "male",
+    },
+    {
+      name: "Blackburn Dotson",
+      friends: ["Jacklyn Lucas", "Linda Chapman"],
+      gender: "male",
+    },
+    {
+      name: "Sheree Anthony",
+      friends: ["Goldie Gentry", "Briana Decker"],
+      gender: "female",
+    },
+  ])
+);
+// [
+//   {
+//     name: "Ross Vazquez",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Carey Barr",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Moore Hensley",
+//     friends: ["Sharron Pace"],
+//     gender: "male"
+//   }
+// ]
--- js/task-4.js
+++ js/task-4.js
@@ -1,32 +1,49 @@
-function getShippingCost(country) {
-  let price;
-  let msg;
-  switch (country) {
-    case "China":
-      price = 100;
-      msg = `Shipping to ${country} will cost ${price} credits`;
-      return msg;
-    case "Chile":
-      price = 250;
-      msg = `Shipping to ${country} will cost ${price} credits`;
-      return msg;
-    case "Australia":
-      price = 170;
-      msg = `Shipping to ${country} will cost ${price} credits`;
-      return msg;
-    case "Jamaica":
-      price = 120;
-      msg = `Shipping to ${country} will cost ${price} credits`;
-      return msg;
-    default:
-      msg = "Sorry, there is no delivery to your country";
-      return msg;
-  }
-}
+const getTotalBalanceByGender = (users, gender) => {
+  return users
+    .filter((user) => user.gender === gender)
+    .reduce((total, user) => {
+      return total + user.balance;
+    }, 0);
+};
 
-console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
-console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
-console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
-console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
-console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
+const clients = [
+  {
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811,
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821,
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793,
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278,
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951,
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498,
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764,
+  },
+];
+
+console.log(getTotalBalanceByGender(clients, "male")); // 12053
+
+console.log(getTotalBalanceByGender(clients, "female")); // 8863
